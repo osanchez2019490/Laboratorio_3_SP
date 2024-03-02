8e0ab47d6833673e37671521630e663acdcfdd88
osanchez <osanchez-2019490@kinal.edu.gt>
2024-03-02 14:34:35 -0600

    Validaciones y middlwares necesarios para el registro

---
+++ src/helpers/db-validator.js
@@ -0,0 +1,15 @@
+import User from '../users/user.js';
+
+export const existingEmail = async(email= '') => {
+    const existingEmail = await User.findOne({email});
+    if(existingEmail) {
+        throw new Error(`The email ${email} does exist in the database`);
+    }
+}
+
+export const existingUsername = async(username = '') => {
+    const existingUsername = await User.findOne([{username}]);
+    if (existingUsername) {
+        throw new Error(`The username ${username} does exist in the database`);
+    }
+}
