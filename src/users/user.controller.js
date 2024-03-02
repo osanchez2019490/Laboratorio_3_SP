e9f9b439524e5c1226254a76290d490d9732dacf
osanchez <osanchez-2019490@kinal.edu.gt>
2024-03-02 16:26:49 -0600

    Se agrega put de user

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -17,3 +17,39 @@ export const userPost = async (req, res) => {
         user
     });
 }
+
+export const putUser = async (req, res = response) => {
+    const {id} = req.params;
+    const { _id, password, role, state, ...resto} = req.body;
+    const token = req.header('x-token');
+    
+    if(!token){
+        return res.status(401).json({ msg:'No token, authorization denied'});
+    }
+
+    const decoded = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+
+    if(!decoded || !decoded.uid || decoded.uid !== id){
+        return res.status(403).json({ msg: 'You do not have permission to update this profile'});
+    }
+
+    if(password){
+        const user = await User.findById(id);
+        const validarPassword = bcrypt.compareSync(password, user);
+
+        if(!validarPassword){
+            return res.status(400).json({ msg: 'The password after is incorrect'});
+        }
+    }
+
+    await User.findByIdAndUpdate(id, resto, {new:true});
+
+    const userPrevious = req.user;
+    const userUpdate = await User.findByIdAndUpdate(id, resto, {new: true});
+
+    res.status(200).json({
+        msg: 'User updated correctly',
+        user: userUpdate,
+        userPrevious
+    })
+}
